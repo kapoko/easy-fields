11b79d5741e4bc8803ec0a714ad3f23968edddab
Kasper Koman <kasper.koman@gmail.com>
2017-02-28 17:39:44 +0100

    Plugin bare basics

--- easy-fields.php
+++ easy-fields.php
@@ -1,82 +1,84 @@
 <?php 
 /*
 Plugin Name: Easy Fields
-Description: Quick and easy custom field creation, directly from template files
+Description: Super easy and programmable custom fields for Wordpress.
 Author: Kasper Koman
 Version: 0.1
-License: Private
+License: GPL-3.0
 */
 
-define( 'EF_VERSION', '0.1' );
+namespace EasyFields;
 
-define( 'EF_PLUGIN', __FILE__ );
+define('EF_VERSION', '0.1');
 
-if ( !class_exists('EasyFields') ) {
+define('EF_PLUGIN', __FILE__);
+
+if (! class_exists('EasyFields')) :
 	
-	class EasyFields {
+class EasyFields 
+{
+	var $settings;
 
-		var $settings;
+	/**
+	 * Construct the plugin object
+	 */
+	public function __construct()
+	{
+		// Settings
+		$this->settings = array(
+			'field_prefix' => 'ef_'
+		);
 
-		/**
-		 * Construct the plugin object
-		 */
-		public function __construct()
-		{
-			// Settings
-			$this->settings = array(
-				'field_prefix' => 'ef_'
-			);
+		// Filters
+		add_filter('ef/get_settings', array($this, 'get_settings'), 1, 1);
+	}
 
-			// Filters
-			add_filter( 'ef/get_settings', array( $this, 'get_settings' ), 1, 1);
+	/**
+	 * Activate the plugin
+	 */
+	public static function activate()
+	{
 
+	}
+
+	/**
+	 * Deactivate the plugin
+	 */
+	public static function deactivate()
+	{
+
+	}
+
+	/**
+	 * Returns the plugins settings
+	 * 
+	 * @return 	mixed 	Returns array of settings or a single setting
+	 */
+	public function get_settings($setting) 
+	{
+		$result = false;
+
+		// If a single setting is chosen, only choose that setting
+		if (isset($this->settings[$setting])) {
+			$result = $this->settings[$setting];
+		} else if ($setting = 'all') {			
+			$result = $this->settings; 
 		}
 
-		/**
-		 * Activate the plugin
-		 */
-		public static function activate()
-		{
-
-		}
-
-		/**
-		 * Deactivate the plugin
-		 */
-		public static function deactivate()
-		{
-
-		}
-
-		/**
-		 * Returns the plugins settings
-		 * 
-		 * @return 	mixed 	Returns array of settings without arguments, or a single setting
-		 */
-		public function get_settings( $s ) {
-			$return = false;
-
-			// If a single setting is chosen, only choose that setting
-			if ( isset( $this->settings[ $s ] ) ) {
-				$return = $this->settings[ $s ];
-
-			// If all is given as argument, return all settings
-			} else if ( $s = 'all' ) {			
-				$return = $this->settings; 
-			}
-
-			return $return;
-		}
+		return $result;
 	}
 }
 
+endif; // End class_exists check
+
+/**
+ * Create instance of a class with activation and deactivation hooks
+ */
 if ( class_exists('EasyFields') ) {
 	global $ef;
 
-    // Installation and uninstallation hooks
     register_activation_hook( EF_PLUGIN, array( 'EasyFields', 'activate' ) );
     register_deactivation_hook( EF_PLUGIN, array( 'EasyFields', 'deactivate' ) );
 
-    // Instantiate the plugin class
     $ef = new EasyFields();
 }
